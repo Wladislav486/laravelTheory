ea642454c870fe8852dd796804d82f9bac44c72c
gorbunov <v.gorbunov@studiofact.ru>
2022-10-22 21:35:47 +0500

    theme 'Routing'

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,80 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', function (){
+//    return '<h1>hello world</h1>';
+//});
+
+//Route::get('/', function (){
+//    $res = 2+3;
+//    $name = 'John';
+//    return view('home', ['name' => $name])->with('res', $res);
+//});
+
+//Route::get('/about', function (){
+//    return '<h1>About</h1>';
+//});
+
+/**
+ * Обработка http запросов
+ */
+//Route::get('/contact', function (){
+//    return view('contact');
+//});
+//
+//
+//Route::post('/send-email', function (){
+//    if(!empty($_POST)){
+//        dump($_POST);
+//    }
+//    return 'send';
+//});
+
+
+
+/**
+ * обработка сразу нескольких видов http запросов
+ */
+//Route::match(['post', 'get'],'/contact', function (){
+//    if(!empty($_POST)){
+//        dump($_POST);
+//    }
+//    return view('contact');
+//});
+
+//Route::any('/contact', function (){
+//    if(!empty($_POST)){
+//        dump($_POST);
+//    }
+//    return view('contact');
+//});
+
+
+/**
+ * именнованые маршруты
+ * централизация изменения url
+ */
+//Route::match(['post', 'get'],'/contact', function (){
+Route::match(['post', 'get'],'/contact2', function (){
+    if(!empty($_POST)){
+        dump($_POST);
+    }
+    return view('contact');
+})->name('contact');
+
+
+/**
+ * роутинг статических страниц
+ */
+Route::view('/test', 'test', ['test' => 'Test data']);
+
+
+/**
+ * редиректы
+ */
+Route::redirect('/about', '/contact2', 301);
+
+
+
+
