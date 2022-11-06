8dd30a7e0436e21eb8c660f9fdb5fc724d3a40b4
gorbunov <vladgorbunov486@gmail.com>
2022-11-06 16:53:38 +0500

    Выполнение SQL-запросов.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,18 +4,51 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
 
     public function index()
     {
+        /**
+         * порядковые макросы - ?
+         * для обработки sql инъекций
+         * return bool
+         */
+//        DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['Статья 4', 'Контент статьи 4']);
 
-        dump($_ENV['MY_SETTING']);
-        dump(env('MY_SETTING'));
-        dump(config('app.timezone'));
-        dump(config('database.connections.mysql.database'));
+        /**
+         * return int count update rows
+         */
+//            DB::update("UPDATE posts SET title = :title, content = :content WHERE id=5", ['title' => 'Статья 5', 'content' => "Контент статьи 5"]);
 
-        return view('home_1', ['res' => 5, 'name' => 'Jhon']);
+        /**
+         * удаление записи
+         */
+        //DB::delete("DELETE FROM posts WHERE id=:id", ['id' => 5]);
+
+        /**
+         * транзакции
+         * Обновить оба поля либо ни одного
+         */
+        DB::beginTransaction();
+        try{
+            DB::update("UPDATE posts SET created_at = ? WHERE created_at IS NULL", [NOW()]);
+            DB::update("UPDATE posts SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
+            DB::commit();
+        }catch (\Exception $e){
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+
+
+
+        /**
+         *  получение элементов - id
+         * именнованные макросы
+         */
+        return DB::select("SELECT * FROM posts");
     }
 
 
