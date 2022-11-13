96f78bc53ba59cdf91cc682ec80cdcfd59c0b58f
gorbunov <vladgorbunov486@gmail.com>
2022-11-13 15:33:48 +0500

    Шаблоны Blade. Часть 4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,61 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Document</title>
-</head>
-<body>
-<h1>Список постов</h1>
 
+@extends('layouts.layout')
+
+@section('title')@parent {{$title}} @endsection
+
+@section('header')
+    @parent
+@endsection
+
+
+
+
+
+
+@section('content')
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+
+            @foreach($posts as $post)
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body">
+                        <h5 class="card-body">{{$post->title}}</h5>
+                        <p class="card-text">{{$post->content}}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                            <small
+                                class="text-muted">
+{{--                                {{$post->created_at}}--}}
+                            {{--    {{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $post->created_at)->format('d.m.Y')}}--}}
+                                {{$post->getPostDate()}}
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
+
+
+
+
+
+
+
+
+
+
+{{--
 <ul>
     <li>
         <a href="{{
@@ -42,6 +89,5 @@
         </form>
     </li>
 </ul>
+--}}
 
-</body>
-</html>
