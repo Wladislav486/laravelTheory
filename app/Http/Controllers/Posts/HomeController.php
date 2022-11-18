e925c802c72d3eec4e0f42fb279d2812fb79927e
gorbunov <vladgorbunov486@gmail.com>
2022-11-18 21:46:55 +0500

    Seeding.

--- app/Http/Controllers/Posts/HomeController.php
+++ app/Http/Controllers/Posts/HomeController.php
@@ -87,7 +87,7 @@ class HomeController extends Controller
         /**
          * пагинация
          */
-        $posts = Post::orderBy('id', 'desc')->paginate(1);
+        $posts = Post::orderBy('id', 'desc')->paginate(3);
 
 
         $title = 'Posts list';
