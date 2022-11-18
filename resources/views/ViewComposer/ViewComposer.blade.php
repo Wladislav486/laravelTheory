3825fafda88cbe8536c9c267d4f6077e0e88aabb
gorbunov <vladgorbunov486@gmail.com>
2022-11-18 20:02:20 +0500

    View Composers.

---
+++ resources/views/ViewComposer/ViewComposer.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('title')@parent ViewComposer @endsection
+
+@section('header')
+    @parent
+@endsection
+
+@section('content')
+    <div class="container">
+    </div>
+@endsection
+
